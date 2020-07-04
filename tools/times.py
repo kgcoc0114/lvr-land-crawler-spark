4fe5751bc67a614653e1483d44e9bd9755adc25b
kgcoc <kgcoc0114@gmail.com>
2020-07-04 14:55:26 +0800

    [Add] trans to AD function

--- tools/times.py
+++ tools/times.py
@@ -19,3 +19,15 @@ def datetime2str(datetime_in, dt_format="%Y%m%d"):
 # 字串轉datetime
 def str2datetime(date_str, dt_format="%Y-%m-%d"):
     return datetime.datetime.strptime(date_str, dt_format)
+
+def dt2AD(date_str, to_dt_format=None):
+    year = int(date_str[0:3]) + 1911
+    month = date_str[3:5]
+    day = date_str[5:]
+    if to_dt_format:
+        return year, month, day
+    return "{}-{}-{}".format(year, month, day)
+
+def dt2AD_dt(date_str):
+    year, month, day = dt2AD(date_str, to_dt_format=True)
+    return str2datetime("{}-{}-{}".format(year,month,day))

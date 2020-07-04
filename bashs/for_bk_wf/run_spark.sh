23d421efd09e4994b51cfacf18ed0a46cdae4270
kgcoc <kgcoc0114@gmail.com>
2020-07-04 15:12:31 +0800

    [Fixed] rename

--- bashs/for_bk_wf/run_spark.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-PROJ="lvr-land-crawler-spark"
-
-export PYSPARK_PYTHON=$HOME/$PROJ/.venv/bin/python; export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64; /usr/local/spark/bin/spark-submit --py-files $HOME/$PROJ/dependency.zip $HOME/$PROJ/backup/land_data_etl.py
